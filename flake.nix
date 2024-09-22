4750a76428216f29c61a478a7471d9debe5af627
jonboh <jon.bosque.hernando@gmail.com>
2024-09-22 17:01:15 +0200

    fix nix build CI

--- flake.nix
+++ flake.nix
@@ -5,47 +5,48 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        rustVersion = pkgs.rust-bin.stable.latest.default;
-        toml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {inherit system overlays;};
+      rustVersion = pkgs.rust-bin.stable.latest.default;
+      toml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
 
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = rustVersion;
-          rustc = rustVersion;
-        };
+      rustPlatform = pkgs.makeRustPlatform {
+        cargo = rustVersion;
+        rustc = rustVersion;
+      };
 
-        myRustBuild = rustPlatform.buildRustPackage {
-          pname =
-            toml.package.name; # make this what ever your cargo.toml package.name is
-          version = "v${toml.package.version}";
-          src = ./.; # the folder with the cargo.toml
+      youtube-tui = rustPlatform.buildRustPackage {
+        pname =
+          toml.package.name; # make this what ever your cargo.toml package.name is
+        version = "v${toml.package.version}";
+        src = ./.; # the folder with the cargo.toml
 
-          cargoLock.lockFile = ./Cargo.lock;
+        cargoLock.lockFile = ./Cargo.lock;
 
-          buildInputs = with pkgs; [
-            openssl
-            xorg.libxcb
-            libsixel
-            mpv
-          ];
+        buildInputs = with pkgs; [
+          openssl
+          xorg.libxcb
+          libsixel
+          mpv
+        ];
 
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            python3
-          ];
-        };
-
-      in
-      {
-        defaultPackage = myRustBuild;
-        devShell = pkgs.mkShell {
-          buildInputs =
-            [ (rustVersion.override { extensions = [ "rust-src" ]; }) ];
-        };
-      });
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          python3
+        ];
+      };
+    in {
+      defaultPackage = youtube-tui;
+      devShell = pkgs.mkShell {
+        buildInputs = [(rustVersion.override {extensions = ["rust-src"];})];
+      };
+    });
 }
-
